020eaf7ccd4849ba6d36823b3225284e185e8343
Sven Steudte <sven.steudte@gmail.com>
2024-07-06 18:08:33 +0200

    Removed build files

--- software/.dep/vectors.o.d
+++
@@ -1,6 +0,0 @@
-build/obj/vectors.o: \
- ChibiOS/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
- ChibiOS/os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h \
- board/board.h
-ChibiOS/os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h:
-board/board.h:
